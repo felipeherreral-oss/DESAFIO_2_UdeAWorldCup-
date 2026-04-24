c5116a5ea7180368d847b508ce92bcb1e2075e86
RigoBerrio <80019602+RigoBerrio@users.noreply.github.com>
2026-04-23 22:43:21 -0500

    main prueba para validar la simulación del partido

--- main.cpp
+++ main.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "estructuras/ArregloDinamico.h"
 #include "entidades/Equipo.h"
 #include "persistencia/GestorArchivos.h"
+#include "simulacion/Partido.h"
 using namespace std;
 
 int main() {
+    srand(time(nullptr)); // semilla aleatoria
 
-    cout << "=== PRUEBA GestorArchivos ===" << endl;
-
+    // Cargar equipos reales
     ArregloDinamico<Equipo> equipos(48);
-
-    // Ajusta la ruta según tu proyecto
     GestorArchivos::cargarEquiposDesdeCSV(equipos,
                                           "datos/selecciones_clasificadas_mundial.csv");
 
-    cout << "Total equipos: " << equipos.getTamanio() << endl; // 48
+    cout << "\n=== PRUEBA Partido ===" << endl;
 
-    // Mostrar primeros 5 equipos
-    cout << "\n--- Primeros 5 equipos ---" << endl;
-    for (int i = 0; i < 5; i++) {
-        equipos[i].imprimir();
-        cout << " | PJ:" << equipos[i].getEstHistorica().getPartidosJugados()
-             << " GF:"   << equipos[i].getEstHistorica().getGolesAFavor()
-             << " GC:"   << equipos[i].getEstHistorica().getGolesEnContra()
-             << endl;
-    }
+    // Partido Argentina vs France
+    Partido p(
+        Fecha(20, 6, 2026),
+        "nombreSede",
+        "codArbitro1", "codArbitro2", "codArbitro3",
+        &equipos[0],  // France (ranking 1)
+        &equipos[2]   // Argentina (ranking 3)
+        );
 
-    // Verificar jugadores del primer equipo
-    cout << "\n--- Jugadores de " << equipos[0].getPais() << " ---" << endl;
-    int totalGoles = 0;
-    for (int j = 0; j < equipos[0].getPlantilla().getTamanio(); j++) {
-        totalGoles += equipos[0].getPlantilla()[j].getEstHistorica().getGoles();
-    }
-    cout << "Total jugadores: "
-         << equipos[0].getPlantilla().getTamanio() << endl; // 26
-    cout << "Total goles repartidos: " << totalGoles
-         << " (debe ser "
-         << equipos[0].getEstHistorica().getGolesAFavor()
-         << ")" << endl;
+    cout << "Antes de simular:" << endl;
+    p.imprimir();
 
-    // Guardar jugadores
-    cout << "\n--- Guardando jugadores ---" << endl;
-    GestorArchivos::guardarJugadores(equipos, "datos/jugadores.csv");
+    p.simular();
 
-    // Recargar jugadores
-    cout << "\n--- Recargando jugadores ---" << endl;
-    GestorArchivos::cargarJugadores(equipos, "datos/jugadores.csv");
-    cout << "Primer jugador de "
-         << equipos[0].getPais() << ": "
-         << equipos[0].getPlantilla()[0].getNombre() << " "
-         << equipos[0].getPlantilla()[0].getApellido() << endl;
+    cout << "\nDespues de simular:" << endl;
+    p.imprimir();
+    p.imprimirGoleadores();
 
-    cout << "\nPrueba GestorArchivos completada." << endl;
+    // Verificar que los historicos se actualizaron
+    p.actualizarHistoricos();
+    cout << "\nHistoricos France tras partido:" << endl;
+    equipos[0].getEstHistorica().imprimir(); cout << endl;
+    cout << "Historicos Argentina tras partido:" << endl;
+    equipos[2].getEstHistorica().imprimir(); cout << endl;
+
+    // Usar operator
+    cout << "\nOperador <<: " << p << endl;
+
     return 0;
 }
