65776c0fecd94b753dffd87f4661f1259d75458e
RigoBerrio <80019602+RigoBerrio@users.noreply.github.com>
2026-04-26 16:52:29 -0500

    Implementación del punto de entrada del sistema
    Se define la lógica principal de ejecución del programa, inicialización del torneo y flujo general de la simulación

--- main.cpp
+++ main.cpp
@@ -1,63 +1,9 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include "estructuras/ArregloDinamico.h"
-#include "entidades/Equipo.h"
-#include "persistencia/GestorArchivos.h"
-#include "simulacion/Grupo.h"
+#include "simulacion/Torneo.h"
 using namespace std;
 
 int main() {
-    srand(time(nullptr));
-
-    ArregloDinamico<Equipo> equipos(48);
-    GestorArchivos::cargarEquiposDesdeCSV(equipos,
-                                          "datos/selecciones_clasificadas_mundial.csv");
-
-    cout << "\n=== PRUEBA Grupo ===" << endl;
-
-    // Crear grupo A con 4 equipos reales
-    Grupo grupoA('A');
-    grupoA.agregarEquipo(&equipos[0]);  // France
-    grupoA.agregarEquipo(&equipos[2]);  // Argentina
-    grupoA.agregarEquipo(&equipos[5]);  // Brazil
-    grupoA.agregarEquipo(&equipos[10]); // Morocco
-
-    // Array global de partidos por día (19 días)
-    int partidosPorDia[19] = {0};
-
-    grupoA.generarPartidos(Fecha(20, 6, 2026), partidosPorDia, 19);
-
-    cout << "Partidos generados: "
-         << grupoA.getPartidos().getTamanio() << endl; // 6
-
-    // Verificar fechas
-    cout << "Fechas asignadas:" << endl;
-    for (int i = 0; i < grupoA.getPartidos().getTamanio(); i++) {
-        cout << "  Partido " << i+1 << ": ";
-        grupoA.getPartidos()[i].getFecha().imprimir();
-        cout << " — "
-             << grupoA.getPartidos()[i].getEquipo1()->getPais()
-             << " vs "
-             << grupoA.getPartidos()[i].getEquipo2()->getPais()
-             << endl;
-    }
-
-    // Simular
-    grupoA.simularPartidos();
-    grupoA.construirTabla();
-
-    // Imprimir tabla
-    grupoA.imprimirTabla();
-
-    // Imprimir partidos con goleadores
-    grupoA.imprimirPartidos();
-
-    cout << "\nClasificados:" << endl;
-    cout << "1ro: " << grupoA.getPrimero()->getPais() << endl;
-    cout << "2do: " << grupoA.getSegundo()->getPais() << endl;
-    cout << "3ro: " << grupoA.getTercero()->getPais() << endl;
-    cout << "4to: " << grupoA.getCuarto()->getPais()  << endl;
-
+    Torneo torneo;
+    torneo.ejecutarMenu();
     return 0;
 }
